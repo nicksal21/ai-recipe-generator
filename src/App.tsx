609d002ab18f876f1af13ffda5c6c33c1be34f46
nicksal21 <nicholas.salazar292@gmail.com>
2026-04-03 17:18:15 -0400

    Connect to Bedrock

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,81 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
+import { FormEvent, useState } from "react";
+import { Loader, Placeholder } from "@aws-amplify/ui-react";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
-
+import outputs from "../amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+Amplify.configure(outputs);
+const amplifyClient = generateClient<Schema>({
+  authMode: "userPool",
+});
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+  const [result, setResult] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const formData = new FormData(event.currentTarget);
+      const { data, errors } = await
+        amplifyClient.queries.askBedrock({
+          ingredients: [formData.get("ingredients")?.toString() || ""],
+        });
+      if (!errors) {
+        setResult(data?.body || "No data returned");
+      } else {
+        console.log(errors);
+      }
+    } catch (e) {
+      alert(`An error occurred: ${e}`);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+    <div className="app-container">
+      <div className="header-container">
+        <h1 className="main-header">
+          Meet Your Personal
+          <br />
+          <span className="highlight">Recipe AI</span>
+        </h1>
+        <p className="description">
+          Simply type a few ingredients using the format ingredient1,
+          ingredient2, etc., and Recipe AI will generate an all-new
+          recipe on
+          demand...
+        </p>
       </div>
-    </main>
+      <form onSubmit={onSubmit} className="form-container">
+        <div className="search-container">
+          <input
+            type="text"
+            className="wide-input"
+            id="ingredients"
+            name="ingredients"
+            placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
+          />
+          <button type="submit" className="search-button">
+            Generate
+          </button>
+        </div>
+      </form>
+      <div className="result-container">
+        {loading ? (
+          <div className="loader-container">
+            <p>Loading...</p>
+            <Loader size="large" />
+            <Placeholder size="large" />
+            <Placeholder size="large" />
+            <Placeholder size="large" />
+          </div>
+        ) : (
+          result && <p className="result">{result}</p>
+        )}
+      </div>
+    </div>
   );
 }
-
 export default App;
